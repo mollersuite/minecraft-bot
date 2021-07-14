0bdc972d72ad0e067814fc2f37eba4349fa396e8
Jack <git@5079.ml>
2021-07-14 00:56:34 -0400

    okay idk

--- index.js
+++ index.js
@@ -1,24 +1,20 @@
 import mineflayer from 'mineflayer'
-import data from 'minecraft-data'
-
 import vec from 'vec3'
+
 const { createBot } = mineflayer
 
 const bot = createBot({
-  port: 10000,
-  host: '95.111.249.143', // https://webclient.prismarine.js.org/
+  port: 64200,
+  host: 'localhost', // https://webclient.prismarine.js.org/
   username: 'mollerbot',
   mainHand: 'left',
   viewDistance: 'far'
 })
 
 function jesus() {
-  if (
-    ['water', 'lava'].includes(
-      bot.blockAt(bot.entity.position.minus(vec('0', '1', '0')))?.name
-    )
-  ) {
-    bot.physics.gravity = 0
+  if (['water', 'lava'].includes(bot.blockAt(bot.entity.position)?.name)) {
+    bot.physics.gravity = -0.08
+    bot.entity.position.add(vec('0', '1', '0'))
   } else bot.physics.gravity = 0.08
 }
 
